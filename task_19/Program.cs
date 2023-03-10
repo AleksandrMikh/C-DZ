5c78b13dd36b54546a42711ac8dfb22a3b49bc5d
Aleksandr Mikhailov <mihailov_am@bk.ru>
2023-03-11 01:38:35 +0300

    finished task_19

--- task_19/Program.cs
+++ task_19/Program.cs
@@ -9,9 +9,23 @@
 Console.Write("Введите пятизначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
+if(number < 10000 || number > 99999)
+{
+    Console.WriteLine("Ввели НЕпятизначное число.");
+}
+else
+{
 int numberA = number / 10000;
-Console.Write($"{numberA}" );
-int numberBb = number / 1000;
-int numberB = numberBb %10;
+int numberB = number /1000 %10;
+int numberC = number /10 %10;
+int numberD = number %10;
 
-Console.Write($"{numberB}" );
+if(numberA == numberD && numberB ==numberC)
+{
+    System.Console.WriteLine("Да, палиндром");
+}
+else
+{
+    System.Console.WriteLine("Нет");
+}
+}
